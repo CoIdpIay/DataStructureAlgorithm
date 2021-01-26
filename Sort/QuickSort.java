7cfcce154c281220f5243932ce065389148966a8
CoIdpIay <56506179+CoIdpIay@users.noreply.github.com>
2021-01-26 18:48:48 +0800

    Update QuickSort.java

--- Sort/QuickSort.java
+++ Sort/QuickSort.java
@@ -21,7 +21,7 @@ public class QuickSort {
 //        Date date2 = new Date();
 //        long time2 = date2.getTime();
 //        System.out.printf("运算的时间为%dms", (int) ((time2 - time1)));
-        int[] arr = {1, 3, 2, 4, 6, 5, 8, 9, 7};
+        int[] arr = {3, 1, 2, 4};
         quickSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
@@ -31,16 +31,21 @@ public class QuickSort {
         int temp;
         int l = left;
         int r = right;
+        // 把比pivot大的值放到右边,比pivot小的值放在左边
         while (l < r) {
+            // 在pivot的左边一直找,找到大于等于pivot值
             while (arr[l] < pivot) {
                 l += 1;
             }
+            // 在pivot的右边一直找,找到小于等于pivot值
             while (arr[r] > pivot) {
                 r -= 1;
             }
             if (l == r) {  // 每次当两指针重合(只会在中值处重合)时意味着指针左侧全是小于中值的,指针右侧全是大于中值的
+                // 这个中值所在的任务已经完成,即其左边都小于它,右边都大于它
                 break;
             }
+            // 否则交换
             temp = arr[l];
             arr[l] = arr[r];
             arr[r] = temp;
@@ -53,8 +58,10 @@ public class QuickSort {
             }
         }
         // 再进行左右侧递归前,将两个指针分开
-        l++;
-        r--;
+        if (l == r) { // 一定要加这个判断,上面while循环也会出现r>l的情况
+            l++;
+            r--;
+        }
         // 若左侧值大于1个,则进行左递归
         if (left < r) {
             quickSort(arr, left, r);
